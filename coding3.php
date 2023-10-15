d5dd8b5a606c1e410f302f47dea1b9808727ddaa
bobrm0403 <137739660+bobrm0403@users.noreply.github.com>
2023-10-15 20:36:41 +0700

    Create coding3.php

---
+++ coding3.php
@@ -0,0 +1,11 @@
+<?php
+
+$inputSignal = 12; 
+$gain = 2; 
+
+$outputSignal = $inputSignal * $gain;
+
+echo "Sinyal Masukan: $inputSignal<br>";
+echo "Penguatan Amplifier: $gain<br>";
+echo "Sinyal Keluaran: $outputSignal";
+?>
